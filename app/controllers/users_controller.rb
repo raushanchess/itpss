5f34fabb09bce6566952c0fbebcedcb4e135be07
Raushan Jaiswal <raushanchess@gmail.com>
2014-03-20 16:59:09 +0545

    simple search

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,84 @@
+class UsersController < ApplicationController
+  
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy 
+  before_filter :set_search
+
+  
+ 
+  def search
+     @search = User.search(params[:q]) 
+     @users = @search.result
+  end
+
+  def index  
+         
+    @users = User.paginate(page: params[:page]) 
+  end
+  
+  
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
+  
+  def new  
+    @user = User.new
+  end
+  
+  def create 
+    @user = User.new(user_params)
+    if @user.save 
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update 
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated" 
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end 
+  
+  def destroy    
+    User.find(params[:id]).destroy
+        flash[:success] = "User deleted."
+        redirect_to users_url
+  end
+  
+     
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :skill, :description, :java,:php, :rails, :password, :password_confirmation)
+    end 
+    
+    # Before filters
+
+        
+        
+        def correct_user
+              @user = User.find(params[:id])
+              redirect_to(root_url) unless current_user?(@user)
+            end  
+            
+            def admin_user
+                  redirect_to(root_url) unless current_user.admin?
+                end  
+                
+                def set_search
+                @search= User.search(params[:q])
+                end
+end
